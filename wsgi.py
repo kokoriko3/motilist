bd157b03c0fb4def2f8ea117e1c3af739f907fc2
Ryoga Kawagoe <you@example.com>
2025-11-18 11:29:02 +0900

    ji

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ load_dotenv()
 import os
 from app import create_app, db
 from app.models.user import User
-from app.models.plan import Plan,TransportSnapshot,LodgingSnapshot,Itinerary,ItinerarySlot,Template,ShareLink
+from app.models.plan import Plan,TransportSnapshot,StaySnapshot,Schedule,ScheduleSlot,Template,Share
 from app.models.checklist import Checklist,ChecklistItem,Item,Category
 
 app = create_app()
