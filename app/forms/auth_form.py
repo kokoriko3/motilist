e9a5e1179cfb26ccf35c96172a21fdcc92bf0c34
kokoriko3 <9011.pikun@gmail.com>
2025-11-20 11:01:09 +0900

    修正

--- app/forms/auth_form.py
+++ app/forms/auth_form.py
@@ -2,34 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
-class SignUpForm(FlaskForm):
-    name = StringField(
-        "ユーザ名",
-        validators=[
-            DataRequired("ユーザ名は必須です。"),
-            Length(1, 30, "30文字以内で入力してください。"),
-        ],
-    )
-
-    email = StringField(
-        "メールアドレス",
-        validators=[
-            DataRequired("メールアドレスは必須です。"),
-            Email("メールアドレスの形式で入力してください。"),
-        ],
-    )
-
-    password = PasswordField(
-        "パスワード",
-        validators=[
-            DataRequired("パスワードは必須です。")
-        ],
-    )
-
-    submit = SubmitField("アカウントを作成する")from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
-
 class LoginForm(FlaskForm):
     email = StringField("メールアドレス", validators=[DataRequired(), Email()])
     password = PasswordField("パスワード", validators=[DataRequired()])
