6e80505a28eea3882ff7fcbf20df110ba40bef75
Ryoga Kawagoe <you@example.com>
2025-11-26 13:00:51 +0900

    ji

--- app/routes/plan_routes.py
+++ app/routes/plan_routes.py
@@ -73,8 +73,7 @@ def plan_transit():
 @plan_bp.route("/stay/", methods=["GET", "POST"])
 def stay_select():
     # 共通：どのプランか決める
-    # plan_id = request.args.get("plan_id", type=int)
-    plan_id = 1
+    plan_id = request.args.get("plan_id", type=int)
     if plan_id is None:
         flash("プランが指定されていません。", "error")
         return redirect(url_for("plan.plan_list"))
